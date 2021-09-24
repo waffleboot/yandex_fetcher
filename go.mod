475e2732293937cde62a685578719b78755cc8d1
Andrei Yangabishev <waffleboot@gmail.com>
2021-09-24 18:29:14 +0300

    яндекс

--- go.mod
+++ go.mod
@@ -2,4 +2,12 @@ module github.com/waffleboot/playstation_buy
 
 go 1.17
 
-require github.com/go-chi/chi v1.5.4
+require (
+	github.com/PuerkitoBio/goquery v1.7.1
+	github.com/go-chi/chi v1.5.4
+)
+
+require (
+	github.com/andybalholm/cascadia v1.2.0 // indirect
+	golang.org/x/net v0.0.0-20210614182718-04defd469f4e // indirect
+)
--- pkg/interfaces/public/http/sites.go
+++ pkg/interfaces/public/http/sites.go
@@ -2,9 +2,16 @@ package http
 
 import (
 	"net/http"
+
+	app "github.com/waffleboot/playstation_buy/pkg/application"
 )
 
 func sites(w http.ResponseWriter, r *http.Request) {
 	search := r.URL.Query().Get("search")
+	err := app.Query(search)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
 	w.Write([]byte(search))
 }
