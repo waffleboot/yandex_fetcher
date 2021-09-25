af7d1f2ccb912dd2f6d898c72880d0d823ff36fb
Andrei Yangabishev <waffleboot@gmail.com>
2021-09-25 18:46:50 +0300

    обновил readme

--- pkg/worker/interfaces/private/http/routes.go
+++ pkg/worker/interfaces/private/http/routes.go
@@ -1,7 +1,57 @@
 package http
 
-import "github.com/go-chi/chi"
+import (
+	"github.com/go-chi/chi"
+
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+
+	app "github.com/waffleboot/playstation_buy/pkg/worker/application"
+)
 
 func (e *Endpoint) AddRoutes(router *chi.Mux) {
 	router.Post("/check", e.check)
+}
+
+type Endpoint struct {
+	service *app.Service
+}
+
+func NewEndpoint(s *app.Service) *Endpoint {
+	return &Endpoint{service: s}
+}
+
+type Request struct {
+	Host string `json:"host"`
+	Url  string `json:"url"`
+}
+
+func (e *Endpoint) check(w http.ResponseWriter, r *http.Request) {
+	var req Request
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprintf(w, "unable to read request: %v", err)
+		return
+	}
+	if err := r.Body.Close(); err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprintf(w, "unable to close request: %v", err)
+		return
+	}
+	if err := json.Unmarshal(body, &req); err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprintf(w, "unable to parse request: %v", err)
+		return
+	}
+	count, err := e.service.Benchmark(req.Host, req.Url)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprintf(w, "unable to benchmark request: %v", err)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "%d", count)
 }
