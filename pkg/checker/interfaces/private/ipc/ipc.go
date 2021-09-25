8d9d8ea7f43ea4b7eeca539aeedd5a130adff35c
Andrei Yangabishev <waffleboot@gmail.com>
2021-09-25 23:28:45 +0300

    fix error

--- pkg/checker/interfaces/private/ipc/ipc.go
+++ pkg/checker/interfaces/private/ipc/ipc.go
@@ -16,21 +16,27 @@ func StartEndpoint(s *app.Service, channel chan ChannelItem) {
 		for e := range channel {
 			var i int
 			for i < len(e.Items) {
-				ans, err := s.Benchmark(e.Items[i])
+				ans, err := s.Benchmark(e.Items[i].Host, e.Items[i].Url)
 				i++
 				if err != nil {
 					e.Errc <- err
 					break
 				}
-				e.Done <- ans
+				e.Done <- domain.StatsItem{
+					Host:  e.Items[i].Host,
+					Count: ans,
+				}
 			}
 			for i < len(e.Items) {
-				ans, err := s.Benchmark(e.Items[i])
+				ans, err := s.Benchmark(e.Items[i].Host, e.Items[i].Url)
 				i++
 				if err != nil {
 					continue
 				}
-				e.Done <- ans
+				e.Done <- domain.StatsItem{
+					Host:  e.Items[i].Host,
+					Count: ans,
+				}
 			}
 		}
 	}()
